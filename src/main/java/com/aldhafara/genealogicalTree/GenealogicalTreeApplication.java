b014615378b113087cce6b361a528198e35e8dc1
Marek Nawrocki <aldhafara@onet.pl>
2024-12-05 12:16:41 +0100

    Initial commit

---
+++ src/main/java/com/aldhafara/genealogicalTree/GenealogicalTreeApplication.java
@@ -0,0 +1,13 @@
+package com.aldhafara.genealogicalTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenealogicalTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenealogicalTreeApplication.class, args);
+	}
+
+}
