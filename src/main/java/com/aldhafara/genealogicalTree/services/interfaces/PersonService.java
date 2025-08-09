7636d18bfc3a25090ddce76bf9a139c6281823a4
Marek Nawrocki <aldhafara@onet.pl>
2025-08-09 15:23:34 +0200

    Remove @Service annotations from service interfaces

--- src/main/java/com/aldhafara/genealogicalTree/services/interfaces/PersonService.java
+++ src/main/java/com/aldhafara/genealogicalTree/services/interfaces/PersonService.java
@@ -2,12 +2,10 @@ package com.aldhafara.genealogicalTree.services.interfaces;
 
 import com.aldhafara.genealogicalTree.entities.Person;
 import com.aldhafara.genealogicalTree.exceptions.PersonNotFoundException;
-import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.UUID;
 
-@Service
 public interface PersonService {
     Person save(Person person);
     Person getById(UUID id) throws PersonNotFoundException;
